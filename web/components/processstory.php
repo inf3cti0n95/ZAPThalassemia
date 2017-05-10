53ecd3ec65412fc71bd35a2f42fd20186d8ba233
Viraj Trivedi <Viraj Trivedi>
2017-05-10 16:30:40 +0530

    file upload

--- web/components/processstory.php
+++ web/components/processstory.php
@@ -6,12 +6,29 @@
  
  
     $content = $_REQUEST['content'];
-    $hero = $_REQUEST['heroimg'];
+    $hero = $_FILES['heroimg'];
     $userid = $_REQUEST['user'];
     $title = $_REQUEST['title'];
 
 
-    mysqli_query($conn,"INSERT INTO story_table(u_id_fk,st_title,st_content,st_heroimg) VALUES ($userid,'$title','$content','$hero')");
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["heroimg"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["heroimg"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+
+
+    mysqli_query($conn,"INSERT INTO story_table(u_id_fk,st_title,st_content,st_heroimg) VALUES ($userid,'$title','$content','$target_file')");
     echo "Entered";
 	
  
