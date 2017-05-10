e4311070f803dcb126155083ae61c192ef102837
Viraj Trivedi <Viraj Trivedi>
2017-05-10 16:58:04 +0530

    upload filename

--- web/components/processstory.php
+++ web/components/processstory.php
@@ -9,6 +9,7 @@
     $hero = $_FILES['heroimg'];
     $userid = $_REQUEST['user'];
     $title = $_REQUEST['title'];
+    $mime  = "";
 
     $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -19,6 +20,7 @@ if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
+        $mime = $check['mime'];
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
@@ -42,19 +44,23 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
+
+    $filename = $target_dir . uniqid().$mime; 
+
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $filename)) {
+        // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
 
 
-    mysqli_query($conn,"INSERT INTO story_table(u_id_fk,st_title,st_content,st_heroimg) VALUES ($userid,'$title','$content','$target_file')");
+
+    mysqli_query($conn,"INSERT INTO story_table(u_id_fk,st_title,st_content,st_heroimg) VALUES ($userid,'$title','$content','https://zapthalassaemia.herokuapp.com/components/$filename')");
     echo "Entered";
 	
  
