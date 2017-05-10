c52be8f0d656667f3b6385dd9e7579c266724352
Viraj Trivedi <Viraj Trivedi>
2017-05-10 12:04:05 +0530

    cleardb connection

--- web/components/connection.php
+++ web/components/connection.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = mysqli_connect($servername,$username,"",$dbname);
 
 
-    $servername = 'localhost';
-	$username = 'root';
-	$dbname = 'thalassemia';
-
-    $fname = $_REQUEST['fname'];
-
-    $data = implode(" ",$_REQUEST);
-
-	$conn = mysqli_connect($servername,$username,"",$dbname);
-
-
-    if(!$conn)
+if(!$conn)
 	{
 		die("Connection failed");
 	}
-
+	else
+	{
+		echo "Yay";
+	}
 ?>
