a7a81dc255fb25bff137b1aaa8ef486dfdc2961c
Viraj Trivedi <inf3cti0n95@gmail.com>
2017-05-11 14:26:15 +0530

    preocess tip

--- web/components/processtip.php
+++ web/components/processtip.php
@@ -4,19 +4,10 @@
 
 <?php
  
- function makeDir($path)
-{
-     return is_dir($path) || mkdir($path);
-}
- 
+
     $content = $_REQUEST['content'];
     $userid = $_REQUEST['user'];
-
-
-
     mysqli_query($conn,"INSERT INTO tip_table(u_id_fk,tip_content) VALUES ($userid,'$content'");
     echo "Entered";
-	
- 
  
  ?>
