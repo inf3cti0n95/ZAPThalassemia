fa5eb5d5917ffed79bd8149a59f89b093eedd6fe
Viraj Trivedi <inf3cti0n95@gmail.com>
2017-05-16 16:45:07 +0530

    bug fix

--- web/components/getreportdata.php
+++ web/components/getreportdata.php
@@ -8,13 +8,14 @@
     $x  = $_REQUEST["xaxis"];
     $y = $_REQUEST["yaxis"];
 
-    $q = "SELECT count(md_id), $x from med_data where md_thstatus=$y group by $x";
+    $q = "SELECT count(md_id) AS COUNT, $x from med_data where md_thstatus=$y group by $x";
+    echo $q;
     $echo = array();
 
     if($r = mysqli_query($conn,$q))
     {
         while($row = mysqli_fetch_assoc($r)){
-            $echo[$row[$x]] = $row['count(md_id)'];
+            $echo[$row[$x]] = $row['COUNT'];
         }
 
         echo json_encode($echo);
